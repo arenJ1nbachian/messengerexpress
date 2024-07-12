7f85a9edaadc2e3216c07020f73b93f0a42ed9a1
ArenJinbachian <2036555@cmontmorency.qc.ca>
2024-07-12 00:10:49 -0400

    Implemented specific routes and routerprovider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import "./App.css";
-import NavBar from "./Components/NavBar.jsx";
 import Contacts from "./Layout/Contacts.jsx";
 import Chat from "./Layout/Chat.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Layout/Root.jsx";
+import Requests from "./Layout/Requests.jsx";
+import Archived from "./Layout/Archived.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "chats", element: <Chat /> },
+      { path: "people", element: <Contacts /> },
+      { path: "requests", element: <Requests /> },
+      { path: "archived", element: <Archived /> },
+    ],
+  },
+]);
 
 const App = () => {
-  return (
-    <>
-      <NavBar />
-      <Contacts />
-      <Chat />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
