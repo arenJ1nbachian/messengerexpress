57586ef0331f6efcca15dfd0dc29f14458ab11de
ArenJinbachian <2036555@cmontmorency.qc.ca>
2024-07-15 15:32:47 -0400

    Code refactoring by changing internal css to external css for Chat.jsx

--- src/Layout/Chat.jsx
+++ src/Layout/Chat.jsx
@@ -14,59 +14,18 @@ const Chat = () => {
   const navBar = useContext(NavContext);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        marginLeft: "1vw",
-        width: !navBar.navExpanded ? "34vw" : "30vw",
-        height: "94vh",
-        backgroundColor: "rgb(34,34,34)",
-        border: "2px solid rgb(34,34,34)",
-        borderRadius: "15px",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-          marginLeft: "2vw",
-          marginTop: "1vw",
-        }}
-      >
+    <div className={`chatBox ${navBar.navExpanded ? "expanded" : "default"}`}>
+      <div className="chatBoxHeader">
+        <div className="chatBoxTitle">Chats</div>
         <div
-          style={{
-            width: "60px",
-            height: "60px",
-            fontWeight: "800",
-            fontSize: "35px",
-            fontFamily:
-              "Segoe UI Historic, Segoe UI, Helvetica, Arial, sans-serif",
-            color: "White",
-          }}
-        >
-          Chats
-        </div>
-        <div
+          className={`chatBoxCompose ${
+            composeHover ? "composeBtnHover" : "default"
+          }`}
           onMouseEnter={() => {
             setComposeHover(true);
           }}
           onMouseLeave={() => {
             setComposeHover(false);
-          }}
-          style={{
-            cursor: composeHover === true ? "pointer" : "none",
-            display: "flex",
-            alignItems: "center",
-            marginLeft: "auto",
-            marginRight: "2vw",
-            width: "60px",
-            height: "60px",
-
-            borderRadius: "50%",
-            justifyContent: "center",
           }}
         >
           <Category
@@ -77,40 +36,15 @@ const Chat = () => {
         </div>
       </div>
       <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          marginLeft: "auto",
-          marginRight: "auto",
-          marginTop: "2vh",
-          width: !navBar.navExpanded ? "28vw" : "25vw",
-          height: "5vh",
-          border: "2px solid rgb(58,59,60)",
-          backgroundColor: "rgb(58,59,60)",
-          borderRadius: "75px",
-        }}
+        className={`chatBoxSearch ${
+          navBar.navExpanded ? "expanded" : "default"
+        }`}
       >
-        <div
-          style={{
-            width: "30px",
-            height: "30px",
-            marginLeft: "1vw",
-          }}
-        >
+        <div>
           <Category img={search} width="100%" height="100%" />
         </div>
 
         <input
-          className="input"
-          style={{
-            border: "none",
-            backgroundColor: "transparent",
-            marginLeft: "1%",
-            fontSize: "20px",
-            fontFamily:
-              "Segoe UI Historic, Segoe UI, Helvetica, Arial, sans-serif",
-            color: "rgb(167,170,174)",
-          }}
           type="text"
           autoComplete="off"
           id="userName"
@@ -119,23 +53,9 @@ const Chat = () => {
       </div>
       <ConvoBox />
       <div
+        className={`tryMessengerBox ${hovered ? "hovered" : "default"}`}
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
-        style={{
-          width: "90%",
-          margin: "auto",
-          height: "8vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          fontWeight: "bold",
-          fontSize: "25px",
-          fontFamily:
-            "Segoe UI Historic, Segoe UI, Helvetica, Arial, sans-serif",
-          color: "White",
-          backgroundColor: hovered ? "rgb(51,51,51)" : "transparent",
-          borderRadius: "10px",
-        }}
       >
         Try Messenger for Windows
       </div>
