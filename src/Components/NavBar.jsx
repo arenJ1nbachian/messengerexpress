7c4ca8c8ada99f7eb2ab813f336f85ee9c5a12c7
ArenJinbachian <2036555@cmontmorency.qc.ca>
2024-07-15 16:24:32 -0400

    Code refactoring by changing internal css to external css for NavBar.jsx

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-import { Box } from "@mui/joy";
 import message from "../images/messageIcon.svg";
 import people from "../images/contactsIcon.svg";
 import request from "../images/requestIcon.svg";
@@ -9,6 +8,7 @@ import { NavContext } from "../Contexts/NavContext";
 import Button from "./NavBarButtons/Button";
 import AccButton from "./NavBarButtons/AccButton";
 import Chevron from "./NavBarButtons/Chevron";
+import "./NavBar.css";
 
 const NavBar = () => {
   const navBar = useContext(NavContext);
@@ -17,27 +17,15 @@ const NavBar = () => {
 
   return (
     <>
-      <Box
-        sx={{
-          marginTop: "5vh",
-          paddingLeft: "16px",
-          height: "95vh",
-          display: "flex",
-          gap: "2vh",
-          flexDirection: "column",
-          width: navBar.navExpanded ? "16.5vw" : "3vw",
-        }}
-      >
+      <div className={`navBox ${navBar.navExpanded ? "expanded" : "default"}`}>
         {Array.from([message, people, request, archive], (value, index) => (
           <Button value={value} index={index} buttonText={buttonText} />
         ))}
-        <div
-          style={{ display: "flex", flexDirection: "row", marginTop: "auto" }}
-        >
+        <div className="accBtn">
           <AccButton account={account} />
         </div>
         {!navBar.navExpanded && <Chevron />}
-      </Box>
+      </div>
     </>
   );
 };
