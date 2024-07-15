3ca840e5266ab8f952f259e8a2219fb8d8d8bfe2
ArenJinbachian <2036555@cmontmorency.qc.ca>
2024-07-15 18:37:27 -0400

    Code refactoring by changing internal css to external css for Chevron.jsx

--- src/Components/NavBarButtons/Chevron.jsx
+++ src/Components/NavBarButtons/Chevron.jsx
@@ -3,6 +3,7 @@ import { NavContext } from "../../Contexts/NavContext";
 import Category from "./Category";
 import chevronLeft from "../../images/chevronL.svg";
 import chevronRight from "../../images/chevronR.svg";
+import "./Chevron.css";
 
 const Chevron = () => {
   const navBar = useContext(NavContext);
@@ -13,53 +14,18 @@ const Chevron = () => {
   };
   return (
     <>
-      {navBar.navExpanded && (
-        <div
-          onMouseEnter={() => navBar.setHovered(5)}
-          onMouseLeave={() => navBar.setHovered(-1)}
-          onClick={handleExpanded}
-          style={{
-            display: "flex",
-            backgroundColor:
-              navBar.hovered === 5 ? "rgba(255, 255, 255, 0.1)" : "transparent",
-            width: "3vw",
-            alignContent: "center",
-            justifyContent: "center",
-            alignItems: "center",
-            border: "2px solid transparent",
-            borderRadius: "10px",
-            marginBottom: "2vh",
-          }}
-        >
-          <Category
-            img={navBar.navExpanded === true ? chevronLeft : chevronRight}
-          />
-        </div>
-      )}
-      {!navBar.navExpanded && (
-        <div
-          onMouseEnter={() => navBar.setHovered(5)}
-          onMouseLeave={() => navBar.setHovered(-1)}
-          onClick={handleExpanded}
-          style={{
-            backgroundColor:
-              navBar.hovered === 5 ? "rgba(255, 255, 255, 0.1)" : "transparent",
-
-            width: "50px",
-            height: "50px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            border: "2px solid transparent",
-            borderRadius: "10px",
-            marginBottom: "2vh",
-          }}
-        >
-          <Category
-            img={navBar.navExpanded === true ? chevronLeft : chevronRight}
-          />
-        </div>
-      )}
+      <div
+        className={`chevron ${navBar.hovered === 5 ? "hovered" : ""} ${
+          navBar.navExpanded ? "expanded" : ""
+        }`}
+        onMouseEnter={() => navBar.setHovered(5)}
+        onMouseLeave={() => navBar.setHovered(-1)}
+        onClick={handleExpanded}
+      >
+        <Category
+          img={navBar.navExpanded === true ? chevronLeft : chevronRight}
+        />
+      </div>
     </>
   );
 };
