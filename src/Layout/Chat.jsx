a4f6636980ab2c188e17f4b030407ee7cdea3bdf
ArenJinbachian <2036555@cmontmorency.qc.ca>
2024-07-19 00:15:54 -0400

    Seperated the ChatBox to it's own jsx element

--- src/Layout/Chat.jsx
+++ src/Layout/Chat.jsx
@@ -8,6 +8,7 @@ import "../CSS/ScrollBar.css";
 import ConvoBox from "../Components/ConvoBox/ConvoBox";
 import defaultPicture from "../images/default.svg";
 import send from "../images/send.svg";
+import Chatbox from "./ChatBox";
 
 const Chat = () => {
   const [hovered, setHovered] = useState(false);
@@ -68,33 +69,7 @@ const Chat = () => {
           navBar.navExpanded ? "expanded" : "default"
         }`}
       >
-        <div className="recipient">
-          <div className="uPicture">
-            <Category img={defaultPicture} width="100%" height="100%" />
-          </div>
-          <div className="uInfo">
-            <div className="uName">Aren Jinbachian</div>
-            <div className="uActive">Active 10h ago</div>
-          </div>
-        </div>
-        <div className="chat"></div>
-        <div className="chatInput">
-          <input
-            type="text"
-            autoComplete="off"
-            id="userName"
-            placeholder="Aa"
-          />
-          <div
-            style={{
-              marginLeft: "auto",
-              marginRight: "1vw",
-              cursor: "pointer",
-            }}
-          >
-            <Category img={send} width="100%" height="100%" />
-          </div>
-        </div>
+        <Chatbox />
       </div>
     </div>
   );
---
+++ src/Layout/ChatBox.jsx
@@ -0,0 +1,35 @@
+import Category from "../Components/NavBarButtons/Category";
+import defaultPicture from "../images/default.svg";
+import send from "../images/send.svg";
+import "./ChatBox.css";
+
+const Chatbox = () => {
+  return (
+    <>
+      {" "}
+      <div className="recipient">
+        <div className="uPicture">
+          <Category img={defaultPicture} width="100%" height="100%" />
+        </div>
+        <div className="uInfo">
+          <div className="uName">Aren Jinbachian</div>
+          <div className="uActive">Active 10h ago</div>
+        </div>
+      </div>
+      <div className="chat"></div>
+      <div className="chatInput">
+        <input type="text" autoComplete="off" id="userName" placeholder="Aa" />
+        <div
+          style={{
+            marginLeft: "auto",
+            marginRight: "1vw",
+            cursor: "pointer",
+          }}
+        >
+          <Category img={send} width="100%" height="100%" />
+        </div>
+      </div>
+    </>
+  );
+};
+export default Chatbox;
