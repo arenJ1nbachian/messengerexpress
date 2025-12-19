4fbd4544a6e2848ebd1b7fcbfcb52aaacda6703e
ArenJinbachian <2036555@cmontmorency.qc.ca>
2025-12-18 22:01:01 -0500

    update env variable in auth

--- backend/middleware/authenticateToken.js
+++ backend/middleware/authenticateToken.js
@@ -2,10 +2,11 @@ const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   const jwt = require("jsonwebtoken");
+  const JWT_SECRET = process.env.JWT_SECRET || "secretKey";
 
   if (!token) return res.sendStatus(401);
 
-  jwt.verify(token, "r$WMw32(wH^GaZZ", (err, user) => {
+  jwt.verify(token, JWT_SECRET, (err, user) => {
     if (err) return res.sendStatus(403); // invalid token
     req.user = user;
     next();
